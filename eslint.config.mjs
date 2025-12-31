b86891fdd8f5688d968753aae412172a8516f5b8
raiselfg <idima6335@gmail.com>
2025-12-31 07:01:21 +0300

    nextjs ptoject init + prettier shadcn drizzle better-auth base setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -8,11 +8,26 @@ const eslintConfig = defineConfig([
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
+  {
+    extends: ['next/core-web-vitals', 'prettier'],
+    plugins: ['prettier', 'simple-import-sort'],
+    rules: {
+      'prettier/prettier': 'error',
+
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+
+      'react/jsx-curly-brace-presence': ['error', 'never'],
+      'react/self-closing-comp': 'error',
+
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+    },
+  },
 ]);
 
 export default eslintConfig;
---
+++ lib/auth/better-auth.ts
@@ -0,0 +1,23 @@
+import { betterAuth } from 'better-auth';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { db } from '@/drizzle/index';
+import * as schema from '@/drizzle/schema';
+import { nextCookies } from 'better-auth/next-js';
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: 'pg',
+    schema,
+  }),
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: false,
+  },
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+  },
+  plugins: [nextCookies()],
+});
