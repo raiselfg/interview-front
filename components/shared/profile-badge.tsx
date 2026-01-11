ad314122ee7deb1ab2e792f2346bf457c51d19d5
raiselfg <idima6335@gmail.com>
2026-01-12 01:44:21 +0300

    updated profile badge to client component

--- components/shared/profile-badge.tsx
+++ components/shared/profile-badge.tsx
@@ -1,22 +1,41 @@
+'use client';
+
 import { getSession } from '@/lib/auth/actions/get-session';
 import Link from 'next/link';
 import { Button } from '../ui/button';
 import Image from 'next/image';
 import { APP_ROUTES } from '@/constants';
+import { User } from 'better-auth';
+import { useEffect, useState } from 'react';
+import { Skeleton } from '../ui/skeleton';
 
-export const ProfileBadge = async () => {
-  const session = await getSession();
+export const ProfileBadge = () => {
+  const [user, setUser] = useState<Pick<User, 'name' | 'image'> | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchSession = async () => {
+      const session = await getSession();
+      setUser(session?.user ?? null);
+      setIsLoading(false);
+    };
+    fetchSession();
+  }, []);
+
+  if (isLoading) {
+    return <Skeleton className="w-19.5 h-9" />;
+  }
 
   return (
     <>
-      {session?.user ? (
+      {user ? (
         <Link href={APP_ROUTES.PROFILE}>
-          <div className="flex items-center gap-2">
-            <p>{session.user.name}</p>
-            {session.user.image && (
+          <div className="flex items-center gap-2 w-19.5 h-9 mx-auto">
+            <p>{user.name}</p>
+            {user.image && (
               <Image
-                src={session.user.image}
-                alt={session.user.name}
+                src={user.image}
+                alt={user.name}
                 width={32}
                 height={32}
                 className="rounded-full"
