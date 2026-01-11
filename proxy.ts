e97734ebe9cfbc4cd064713cf9e1cbd3ba8b352b
raiselfg <idima6335@gmail.com>
2026-01-11 23:28:51 +0300

    better usage of getting session in proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,14 +1,11 @@
-import { headers } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
-import { auth } from './lib/auth/better-auth';
+import { getSession } from './lib/auth/actions/get-session';
 
 export async function proxy(request: NextRequest) {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  }); // THIS IS NOT SECURE!
+  const data = await getSession();
   // This is the recommended approach to optimistically redirect users
   // We recommend handling auth checks in each page/route
-  if (!session) {
+  if (!data.session) {
     return NextResponse.redirect(new URL('/auth/login', request.url));
   }
   return NextResponse.next();
